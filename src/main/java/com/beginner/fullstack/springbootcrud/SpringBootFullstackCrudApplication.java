edafcf2d5218e2f9742966705f8d39bfa0bc8477
Shubham Asthana <asthana.shubham@gmail.com>
2019-07-04 16:18:32 +0530

    First Commit

---
+++ src/main/java/com/beginner/fullstack/springbootcrud/SpringBootFullstackCrudApplication.java
@@ -0,0 +1,13 @@
+package com.beginner.fullstack.springbootcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFullstackCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFullstackCrudApplication.class, args);
+	}
+
+}
